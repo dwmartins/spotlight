c6ce67840e5ce724f0f3117001ff546020082647
Douglas Wellington Martins <90710466+dwmartins@users.noreply.github.com>
2025-01-17 21:59:20 -0300

    Aplicando novas configurações

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,7 @@
-import './bootstrap';
+document.addEventListener('DOMContentLoaded', function () {
+    // Initialize all tooltips automatically
+    var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'));
+    var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
+        return new bootstrap.Tooltip(tooltipTriggerEl);
+    });
+});
