e4774103eb22e29eb42d374468b25a2021716732
Douglas Wellington Martins <90710466+dwmartins@users.noreply.github.com>
2025-01-19 22:40:36 -0300

    Alteração na função de redirecionamento para aceitar parametros

--- app/Helpers/Functions.php
+++ app/Helpers/Functions.php
@@ -7,12 +7,13 @@
  * or an associative array with fields as key and messages as value.
  * @param string|null $route (Optional) The route to which the user will be redirected. If not provided,
  * the user will be redirected to the previous page.
+ * @param array $params (option) Is an array of parameters to be passed to the URL.
  */
-function redirectWithMessage($messageType, $title, $messageOrFields, $route = null) {
+function redirectWithMessage($messageType, $title, $messageOrFields, $route = null, $params = []) {
     $message = ['type' => $messageType, 'title' => $title, 'fields' => $messageOrFields];
 
     if($route) {
-        return redirect()->route($route)->with('message', $message);
+        return redirect()->route($route, $params)->with('message', $message);
     }
 
     return back()->withInput()->with('message', $message);
