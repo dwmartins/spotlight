d0006db8f5436ed960ba8b3750b79661aa125ad8
Douglas Wellington Martins <90710466+dwmartins@users.noreply.github.com>
2025-01-24 23:41:13 -0300

    Adicionado o old() no formulario

--- resources/views/forms/user/basicInfo.blade.php
+++ resources/views/forms/user/basicInfo.blade.php
@@ -2,23 +2,23 @@
     @csrf
     <div class="mb-3 col-12 col-md-4">
         <label for="name" class="text-secondary mb-2"><span class="text-danger me-1">*</span>{{ trans('messages.LABEL_NAME') }}</label>
-        <input type="text" name="name" id="name" autocomplete="name" value="{{ auth()->user()->name }}" class="form-control custom_focus text-secondary">
+        <input type="text" name="name" id="name" autocomplete="name" value="{{ old('name', auth()->user()->name) }}" class="form-control custom_focus text-secondary">
     </div>
     <div class="mb-3 col-12 col-md-4">
         <label for="lastName" class="text-secondary mb-2"><span class="text-danger me-1">*</span>{{ trans('messages.LABEL_LAST_NAME') }}</label>
-        <input type="text" name="lastName" id="lastName" autocomplete="lastName" value="{{ auth()->user()->lastName }}" class="form-control custom_focus text-secondary">
+        <input type="text" name="lastName" id="lastName" autocomplete="lastName" value="{{ old('lastName', auth()->user()->lastName) }}" class="form-control custom_focus text-secondary">
     </div>
     <div class="mb-3 col-12 col-md-4">
         <label for="dateOfBirth" class="text-secondary mb-2">{{ trans('messages.DATE_BIRTH') }}</label>
-        <input type="date" name="dateOfBirth" id="dateOfBirth" value="{{ auth()->user()->dateOfBirth }}" class="form-control custom_focus text-secondary">
+        <input type="date" name="dateOfBirth" id="dateOfBirth" value="{{ old('dateOfBirth', auth()->user()->dateOfBirth) }}" class="form-control custom_focus text-secondary">
     </div>
     <div class="mb-3 col-12 col-md-6">
         <label for="email" class="text-secondary mb-2"><span class="text-danger me-1">*</span>{{ trans('messages.LABEL_EMAIL') }}</label>
-        <input type="email" name="email" id="email" autocomplete="email" value="{{ auth()->user()->email }}" class="form-control custom_focus text-secondary">
+        <input type="email" name="email" id="email" autocomplete="email" value="{{ old('email', auth()->user()->email) }}" class="form-control custom_focus text-secondary">
     </div>
     <div class="mb-3 col-12 col-md-6">
         <label for="phone" class="text-secondary mb-2">{{ trans('messages.LABEL_PHONE') }}</label>
-        <input type="number" name="phone" id="phone" autocomplete="phone" value="{{ auth()->user()->phone }}" class="form-control custom_focus text-secondary">
+        <input type="number" name="phone" id="phone" autocomplete="phone" value="{{ old('phone', auth()->user()->phone) }}" class="form-control custom_focus text-secondary">
     </div>
     <div class="mb-3 col-12">
         <label for="phone" class="text-secondary mb-2">{{ trans('messages.LABEL_DESCRIPTION') }}</label>
@@ -28,7 +28,7 @@
             :maxChars="500" 
             placeholder="Digite aqui sua descrição..."
             rows='4'
-            value="{{ auth()->user()->description }}"
+            value="{{ old('description', auth()->user()->description) }}"
         />
     </div>
 
