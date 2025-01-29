9ed10624dfec4f554dc9e9841146f5603dca6300
Douglas Wellington Martins <90710466+dwmartins@users.noreply.github.com>
2025-01-29 18:56:51 -0300

    Alteração na cor do nome do usuário

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -157,7 +157,7 @@
 
                         <a class="nav-link nav-profile d-flex align-items-center pe-0" href="javascript:void(0);" data-bs-toggle="dropdown">
                             <img src="{{ auth()->user()->getAvatar() }}" alt="Profile">
-                            <span class="d-none d-md-block dropdown-toggle ps-2 fs-6">{{ auth()->user()->name }}</span>
+                            <span class="d-none d-md-block dropdown-toggle ps-2 fs-6 custom-text-white">{{ auth()->user()->name }}</span>
                         </a><!-- End Profile Iamge Icon -->
 
                         <ul class="dropdown-menu dropdown-profile shadow custom-bg-white">
