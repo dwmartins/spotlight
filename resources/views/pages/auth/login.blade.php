d8b07759de6547b8871bc3cb64207cac9149cc97
Douglas Wellington Martins <90710466+dwmartins@users.noreply.github.com>
2025-01-29 22:44:12 -0300

    Alteração no layout da página de login

--- resources/views/pages/auth/login.blade.php
+++ resources/views/pages/auth/login.blade.php
@@ -1,56 +1,48 @@
 @extends('layouts.base')
 
 @section('content')
-    <section class="publicLoginView container item_center vh-100">
-        <div class="row w-100">
-            <div class="col-sm-12 col-md-6 col-xl-6 d-flex justify-content-center align-items-center" data-aos="fade-right">
-                <form action="/login" method="post" class="form_login w-100">
-                    @csrf
-                    <div class="website_logo item_center mb-4">
-                        <img src="{{ config('website_info.logoImage') }}" alt="Logo">
-                    </div>
+    <section class="publicLoginView container-fluid item_center vh-100 bg-gray-100 position-relative">
+        <div class="cover-image-login rounded-4" style="background-image: url({{ asset('assets/images/cover-image-login.jpg') }})"></div>
 
-                    <div class="mb-4">
-                        <h4 class="fw-bold custom_dark">{{ trans('messages.TITLE_LOGIN') }}</h4>
-                        <p>{{ trans('messages.ENTER_YOUR_EMAIL_AND_PASSWORD') }}</p>
-                    </div>
+        <form action="/login" method="post" class="form_login w-100 bg-white p-4 rounded-4 position-relative z-1">
+            @csrf
 
-                    <div class="mb-3">
-                        <input type="email" name="email" id="email" autocomplete="email" placeholder="{{ trans('messages.LABEL_EMAIL') }}" class="form-control form-control-lg custom_focus text-secondary fs-6" value="{{ old('email') }}">
-                    </div>
-
-                    <div class="mb-4">
-                        <div class="position-relative">
-                            <input type="password" name="password" id="password" placeholder="{{ trans('messages.LABEL_PASSWORD') }}" class="form-control form-control-lg custom_focus text-secondary fs-6">
-                            <i class="fa-regular icon_show_password fa-eye text-secondary"></i>
-                        </div>
-                    </div>
-
-                    <div class="d-flex justify-content-between align-items-center gap-1 mb-4 px-1">
-                        <label for="rememberMe" class="d-flex align-items-center gap-2 form-check-label text-secondary fs-7 cursor_pointer">
-                            <input class="switch" name="rememberMe" id="rememberMe" type="checkbox">
-                            {{ trans('messages.REMEMBER_ME') }}
-                        </label>
-                        <a href="/" class="link-primary outline_none fs-7">{{ trans('messages.FORGOT_PASSWORD') }}</a>
-                    </div>
-
-                    <button id="btnLogin" class="btn btn-primary btn-lg w-100 fs-6" data-trans-loading="{{ trans('messages.BTN_LABEL_LOADING') }}">
-                        {{ trans('messages.BTN_TEXT_LOGIN') }}
-                        <i class="fa-solid fa-arrow-right-to-bracket"></i>
-                    </button>
-
-                    <hr class="text-secondary">
-
-                    <p class="text-secondary fs-7 text-center">
-                        {{ trans('messages.NOT_HAVE_ACCOUNT') }}
-                        <a href="{{ route('register') }}" class="link-primary outline_none">{{ trans('messages.CREATE_MY_ACCOUNT') }}</a>
-                    </p> 
-                </form>
+            <div class="mb-5">
+                <h4 class="fw-bold custom_dark">{{ trans('messages.WELCOME_BACK') }}</h4>
+                <p>{{ trans('messages.ENTER_YOUR_EMAIL_AND_PASSWORD') }}</p>
             </div>
-            <div class="col-md-6 col-xl-6 d-none d-md-flex justify-content-center align-items-center" data-aos="fade-left">
-                <img src="{{ asset('assets/svg/login.svg') }}" alt="Login" class="w-100 illustration">
+
+            <div class="mb-3">
+                <input type="email" name="email" id="email" autocomplete="email" placeholder="{{ trans('messages.LABEL_EMAIL') }}" class="form-control form-control-lg custom_focus text-secondary fs-6" value="{{ old('email') }}">
             </div>
-        </div>
+
+            <div class="mb-4">
+                <div class="position-relative">
+                    <input type="password" name="password" id="password" placeholder="{{ trans('messages.LABEL_PASSWORD') }}" class="form-control form-control-lg custom_focus text-secondary fs-6">
+                    <i class="fa-regular icon_show_password fa-eye text-secondary"></i>
+                </div>
+            </div>
+
+            <div class="d-flex justify-content-between align-items-center gap-1 mb-4 px-1">
+                <label for="rememberMe" class="d-flex align-items-center gap-2 form-check-label text-secondary fs-7 cursor_pointer">
+                    <input class="switch" name="rememberMe" id="rememberMe" type="checkbox">
+                    {{ trans('messages.REMEMBER_ME') }}
+                </label>
+                <a href="/" class="link-primary outline_none fs-7">{{ trans('messages.FORGOT_PASSWORD') }}</a>
+            </div>
+
+            <button id="btnLogin" class="btn btn-primary btn-lg w-100 fs-6" data-trans-loading="{{ trans('messages.BTN_LABEL_LOADING') }}">
+                {{ trans('messages.BTN_TEXT_LOGIN') }}
+                <i class="fa-solid fa-arrow-right-to-bracket"></i>
+            </button>
+
+            <hr class="text-secondary">
+
+            <p class="text-secondary fs-7 text-center">
+                {{ trans('messages.NOT_HAVE_ACCOUNT') }}
+                <a href="{{ route('register') }}" class="link-primary outline_none">{{ trans('messages.CREATE_MY_ACCOUNT') }}</a>
+            </p> 
+        </form>
     </section>
 
     @vite('resources/js/auth/index.js')
